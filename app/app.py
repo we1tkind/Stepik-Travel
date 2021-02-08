dea7300f26c447a7a482985dc270f2262835acd7
Alexander Erdyakov <sa@prg.re>
2021-02-08 15:35:54 +0300

    добавил заполнение данными страницы направления

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
-from filters import word_agree_with_number
 from consts import INDEX_TOUR_COUNT
 from data import tours, departures
+from filters import word_agree_with_number, price_limiter, nights_limiter
 
 
 app = Flask(__name__)
 app.jinja_env.filters['word_agree_with_number'] = word_agree_with_number
+app.jinja_env.filters['price_limiter'] = price_limiter
+app.jinja_env.filters['nights_limiter'] = nights_limiter
 
 
 @app.route('/')
@@ -15,9 +17,16 @@ def index(tours=tours):
     return render_template('index.html', tours=tours, departures=departures)
 
 
-@app.route('/departures/<int:departure_id>/')
-def render_departures(departure_id):
-    return render_template('departure.html')
+@app.route('/departures/<departure_tag>/')
+def render_departures(departure_tag, tours=tours):
+    filtered_tours = {k: v for k, v in tours.items()
+                      if v['departure'] == departure_tag}
+    return render_template(
+        'departure.html',
+        tours=filtered_tours,
+        departures=departures,
+        departure_name=departures[departure_tag],
+    )
 
 
 @app.route('/tours/<int:tour_id>/')
